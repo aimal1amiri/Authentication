a0e759d7953ef6a6db7d774f367deff69fbbb313
emir2269 <ali724345@gmail.com>
2024-10-21 23:42:26 +0300

    I created email template in mailttrap software and integrated, when user is verified it sends welcome email ,check -email.js- file. one problem is, user data in database is updating and sending welcome emails are also working but only problem is when in -postman- i click on send the reqest , it doesnt give back the response , the json file . even i wrote the code for response.

--- back-end/mailTrap/email.js
+++ back-end/mailTrap/email.js
@@ -22,3 +22,28 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     }
 
 }
+
+export const sendWelcomeEmail=async(email,name)=>{
+    const recipient=[{email}]
+
+    try {
+        const response=await client.send({
+            from:sender,
+            to:recipient,
+            template_uuid:"5744fbd3-9e9d-4f68-b935-2ad55a7e6fb4",
+            template_variables:{
+                "company_info_name":"Cineos",
+                name:name,
+
+            },
+
+        });
+
+        console.log("Welcome Email sent successfully.",response);
+    } catch (error) {
+
+        console.error(`Error sending Welcome Email`,error);
+        throw new Error(`Error sending Welcome Email: ${error}`);
+        
+    }
+}
