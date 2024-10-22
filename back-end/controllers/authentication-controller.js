03da2a0ade23daf50d1b8ea7fd35e88f6b64b99f
emir2269 <ali724345@gmail.com>
2024-10-22 12:50:52 +0300

    the login endpoint is set up. when user login it assign the token, and when signput endpoint is triggred it will remove the token from user.

--- back-end/controllers/authentication-controller.js
+++ back-end/controllers/authentication-controller.js
@@ -82,7 +82,32 @@ export const verifyEmail = async(req,res) => {
 };
 
 export const login = async(req,res) => {
-    res.send("login route")
+
+    const {email, password}=req.body;
+    try {
+        const user= await Userr.findOne({email});
+        
+        if(!user) {
+            return res.status(400).json({success:false,message:"Invalid credentials"});
+        }
+
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+        if(!isPasswordValid) {
+            return res.status(400).json({success:false, message:"Invalid credentialss"});
+        }
+
+        generateTokenAndSetCookie(res,user._id);
+
+        user.lastLogin= new Date();
+
+        await user.save();
+
+        res.status(200).json({success:true, message:"Logged in successfully", user:{...user._doc, password:undefined,},})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({success:false,message:error.message});        
+    }
 }
 
 export const signout = async(req,res) =>{
