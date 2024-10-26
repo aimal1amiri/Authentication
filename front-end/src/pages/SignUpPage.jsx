dc87763cbd6340fdda0eb3711ceb5e518777e6c1
emir2269 <ali724345@gmail.com>
2024-10-26 21:43:28 +0300

    front end designing

--- front-end/src/pages/SignUpPage.jsx
+++ front-end/src/pages/SignUpPage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState } from 'react'
+import { motion} from "framer-motion";
+import {User, Mail, Lock} from "lucide-react";
+import Input from "../components/Input";
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
-}
 
-export default SignUpPage
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = (e) =>{
+    e.preventDefault();
+  }
+
+  return (
+    <motion.div initial={{opacity:0, y:20}} animate={{opacity:1, y:0}} transition={{duration:0.5}} className='max-w-md w-full bg-white backdrop-filter rounded-2xl shadow-xl overflow-hidden'>
+      
+      <div className='p-8'>
+        <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-l from-orange-950 to-amber-500 text-transparent bg-clip-text'>
+          Create Account
+        </h2>
+
+        <form onSubmit={handleSignUp}>
+          <Input icon={User} type='text' placeholder='Full Name' value={name} onChange={(e) => setName(e.target.value)} />
+          <Input icon={Mail} type='email' placeholder='Email Address' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Input icon={Lock} type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+          <motion.button className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg
+          hover:from-green-500 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200' 
+          whileHover={{scale:1.02}} whileTap={{scale:0.98}} type='submit'> Sign Up
+
+          </motion.button>
+
+        </form>
+      </div>
+
+    </motion.div>
+  );
+};
+
+export default SignUpPage;
